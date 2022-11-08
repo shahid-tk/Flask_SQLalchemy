b6fb2d539f033a4a1e9b36ebf6ca9f49a00c9e3d
Shahid Thaikulathil <67534099+shahid-tk@users.noreply.github.com>
2022-11-08 21:44:34 +0300

    Initial commit app.py
    introduction to Flask_SQLAlchemy

---
+++ app.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from flask import Flask
+from flask-SQLAlchemy import SQLalchemy
+ 
+app = Flask(__name__)
+ 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite3:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+ 
+db = SQLalchemy(app)
+ 
+class Table(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.now)
+ 
+@app.route('/<name>')
+def home(name):
+    table = Table(name=name)
+    db.session.add(table)
+    db.session.commit()
+    return '<h1> Name Added: {name} </h1>'
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
