67a5a849e4047807d8a7e870109565c82100efca
Shahid Thaikulathil <67534099+shahid-tk@users.noreply.github.com>
2022-11-11 04:13:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import os
 from datetime import datetime
 from flask import Flask
 from flask-SQLAlchemy import SQLalchemy
+from sqlalchemy.sql import func
+ 
+ basedir = os.path.abspath(os.path.dirname(__file__))
  
 app = Flask(__name__)
  
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite3:///db.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
  
 db = SQLalchemy(app)
